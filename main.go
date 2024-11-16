2b7aedd605f0ed05346c90cb65a61f37be39c928
Alberto Gimenez <agimenez@sysvalve.es>
2024-11-16 15:03:53 +0100

    create hello and goodbye functions

--- main.go
+++ main.go
@@ -7,5 +7,14 @@ import "fmt"
 
 // testing test
 func main() {
+	printHello()
+	printGoodBye()
+}
+
+func printHello() {
 	fmt.Println("Hello World!")
+}
+
+func printGoodBye() {
+	fmt.Println("Goodbye World!")
 }
